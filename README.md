2fc8d1550381e79780f7773255c5bc3464138552
Courtcircuits <tristan-mihai.radulescu@etu.umontpellier.fr>
2024-05-12 23:22:53 +0200

    feat(crud) : create action

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # README
+
+## Architecture
+
+![](architecture.png)
+
 ## Commands done
+
 ```bash
 rails new twitter
 cd twitter
 sudo bundle install
- 
+
 # troubleshooting `bundle install`
 sudo apt install libyaml-dev
 sudo gem install psych
@@ -15,5 +21,6 @@ bin/rails server
 
 # generating posts coontroller
 bin/rails generate controller Tweets index --skip-routes
-
+bin/rails generate model Tweet title:string body:text
+docker compose up -d
 ```
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,37 @@
 class TweetsController < ApplicationController
   def index
+    @tweets = Tweet.all # no clue what the @ is for
+    render json: @tweets
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+    render json: @tweet
+  end
+
+  def create
+    puts "create"
+    begin
+      params = tweet_params
+    rescue => e
+      render json: {error: e.message}, status: :unprocessable_entity
+    end
+    @tweet = Tweet.new(title: params[:title], body: params[:body])
+    if @tweet.save
+      render json: @tweet, status: :created
+    else
+      render json: @tweet.errors, status: :unprocessable_entity
+    end
+  end
+
+  def tweet_params
+    puts "tweet params"
+    puts "params: #{params}"
+    if params[:tweet][:title] == nil
+      raise "Title is required"
+    elsif params[:tweet][:body] == nil
+      raise "Body is required"
+    end
+    return params[:tweet]
   end
 end
